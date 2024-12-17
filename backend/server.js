5ac0cab47c1f789837d0fe6eb57d45bc1b3b484b
vishwajeetshetgaonkar97 <54806391+vishwajeetshetgaonkar97@users.noreply.github.com>
2024-12-17 16:27:24 -0500

    WIP

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,10 @@ app.use(cors({
   origin: "*",
 }));
 
-app.use(bodyParser());
+// app.use(bodyParser());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allowed HTTP methods
