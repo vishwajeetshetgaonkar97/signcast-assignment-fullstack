b237a0806130017c74fa231f93af5d3a1788f8c7
vishwajeetshetgaonkar97 <54806391+vishwajeetshetgaonkar97@users.noreply.github.com>
2024-12-17 15:08:00 -0500

    socket url change

--- web-dashboard/src/Components/FabricCanvas/FabricCanvas.tsx
+++ web-dashboard/src/Components/FabricCanvas/FabricCanvas.tsx
@@ -260,7 +260,7 @@ const FabricCanvas: React.FC<CanvasProps> = ({ fabricCanvasRef }) => {
     updateCanvasSize();
     window.addEventListener('resize', updateCanvasSize);
 
-    websocketRef.current = new WebSocket("ws://signcast-assignment-fullstack-production.up.railway.app/");
+    websocketRef.current = new WebSocket("wss://signcast-assignment-fullstack-production.up.railway.app/");
 
     websocketRef.current.onopen = () => {
       console.log("WebSocket connected");
