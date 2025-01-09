7f9678daf5ab06e2bad9f81c366ca0814002ec06
vishwajeetshetgaonkar97 <54806391+vishwajeetshetgaonkar97@users.noreply.github.com>
2025-01-09 04:45:29 -0500

    added sockets

--- web-dashboard/src/Components/CanvasParentComponent/CanvasParentComponent.tsx
+++ web-dashboard/src/Components/CanvasParentComponent/CanvasParentComponent.tsx
@@ -11,6 +11,7 @@ import CanvasScreensComponent from "../CanvasScreensComponent/CanvasScreensCompo
 import SelectedCanvasObjectIndexDataContext from "../../Contexts/SelectedCanvasObjectIndexDataContext";
 import AllCanvasesDataContext from "../../Contexts/AllCanvasesDataContext";
 import AddToCanvasModal from "../AddCanvasModal/AddCanvasModal";
+import updateCanvas from "../../api/updateCanvas";
 
 interface allcanvases {
   name: string;
@@ -26,6 +27,7 @@ const CanvasParentComponent: React.FC = () => {
   const [selectedCanvasIndex, setSelectedCanvasIndex] = useState<number>(0);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const websocketRef = useRef<WebSocket | null>(null);
 
 
   const getAllCanvases = async () => {
@@ -33,6 +35,27 @@ const CanvasParentComponent: React.FC = () => {
       const data = await getCanvases();
       console.log('canvases', data.canvases);
       setAllCanvases(data.canvases);
+
+      const objects = data.canvases[0].data
+
+      console.log("objects on call", objects)
+
+
+      if (canvas) {
+        canvas.add(...objects);
+        canvas.renderAll();
+      } else {
+        console.log("no canvas, creating a new one");
+        const newCanvas = new fabric.Canvas(canvasRef.current, {
+          width: 1920,
+          height: 1080,
+        });
+        newCanvas.backgroundColor = "#fff";
+        newCanvas.add(...objects);
+        newCanvas.renderAll();
+        setCanvas(newCanvas);
+      }
+
     } catch (error) {
       console.log(`canvas get issue ${error}`);
     }
@@ -48,6 +71,58 @@ const CanvasParentComponent: React.FC = () => {
       initCanvas.renderAll();
       setCanvas(initCanvas);
       getAllCanvases();
+
+      // socket connection 
+
+      // websocketRef.current = new WebSocket("wss://signcast-assignment-fullstack-production.up.railway.app/");
+
+      websocketRef.current = new WebSocket("ws://localhost:3003");
+
+      websocketRef.current.onopen = () => {
+        console.log("WebSocket connected");
+        const data = {
+          name: "Master Device",
+          status: "online",
+        }
+        // sendDeviceMonitoringStatus(data)
+        // setIsMonitoring(true)
+      };
+
+      websocketRef.current.onmessage = (event) => {
+        try {
+          const data = JSON.parse(event.data);
+          console.log("Received data:", data);
+
+
+          if (data.type === "updateAllCanvas") {
+            console.log("Updating canvas objects for all:", data);
+            setAllCanvases(data.canvases);
+            // setCanvasObjects(data.canvases[selectedCanvasIndex].data);
+            // if (!isMonitoring) {
+            //   setIsMonitoring(true)
+            // }
+          } else if (data.type === "notification") {
+            console.log("Notification:", data.message);
+          }
+        } catch (error) {
+          console.error("Error parsing WebSocket message:", error);
+        }
+      };
+
+
+      websocketRef.current.onclose = () => {
+        console.log("WebSocket disconnected");
+        const data = {
+          name: "Master Device",
+          status: "offline",
+        }
+        // sendDeviceMonitoringStatus(data)
+        // setIsMonitoring(false)
+      };
+
+      websocketRef.current.onerror = (error) => {
+        console.error("WebSocket error:", error);
+      }
 
       return () => {
         initCanvas.dispose();
@@ -74,17 +149,13 @@ const CanvasParentComponent: React.FC = () => {
 
   const handleAddCanvas = async (postData) => {
     try {
-     
 
-   
-        await addCanvas(postData);
-        const data = await getCanvases();
-        console.log("dataaa canvass", data);
-        setAllCanvases(data.canvases);
-        setIsModalOpen(false);
-    
+      await addCanvas(postData);
+      const data = await getCanvases();
+      console.log("dataaa canvass", data);
+      setAllCanvases(data.canvases);
+      setIsModalOpen(false);
 
-     
     } catch (error) {
       console.log(`canvas get issue ${error}`);
       alert('Error');
@@ -103,9 +174,29 @@ const CanvasParentComponent: React.FC = () => {
     [selectedCanvasIndex, setSelectedCanvasIndex]
   );
 
+  const handleSyncCanvas = async () => {
+
+    try {
+      const currenObjects = canvas.getObjects()
+      const updateCanvasPostBody = {
+        canvasId: allcanvases[selectedCanvasIndex]._id,
+        name: allcanvases[selectedCanvasIndex].name,
+        category: allcanvases[selectedCanvasIndex].category,
+        data: currenObjects,
+      };
+      console.log("body posted", updateCanvasPostBody)
+      const log = await updateCanvas(updateCanvasPostBody);
+      console.log("log addition", log);
+      // setIsSyncRequired(false);
+    } catch (error) {
+      console.log(`canvas sync issue ${error}`);
+      alert('Error to sync canvas');
+    }
+  };
 
 
   console.log("allcanvases", allcanvases);
+  console.log("canvas", canvas)
 
   return (
     <AllCanvasesDataContext.Provider value={allCanvasDataContextValue}>
@@ -136,8 +227,13 @@ const CanvasParentComponent: React.FC = () => {
 
 
             {isModalOpen && (
-             <AddToCanvasModal handleAddCanvas={handleAddCanvas} setIsModalOpen={setIsModalOpen}/>
+              <AddToCanvasModal handleAddCanvas={handleAddCanvas} setIsModalOpen={setIsModalOpen} />
             )}
+
+            <div className="flex flex-row items-center justify-center absolute z-10 bottom-15 right-2 " onClick={handleSyncCanvas}>
+              <button>Sync</button>
+            </div>
+
 
           </div>
         </>
