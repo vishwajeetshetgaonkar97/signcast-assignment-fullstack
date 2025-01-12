9f49401fe732d90b5361919b25c9b79e189a3eac
vishwajeetshetgaonkar97 <54806391+vishwajeetshetgaonkar97@users.noreply.github.com>
2025-01-12 03:38:09 -0500

    minor tweaks

--- desktop-application/src/Components/CanvasZoomInOutComponent/CanvasZoomInOutComponent.tsx
+++ desktop-application/src/Components/CanvasZoomInOutComponent/CanvasZoomInOutComponent.tsx
@@ -33,7 +33,7 @@ const CanvasZoomInOutComponent: React.FC<CanvasZoomInOutComponentProps> = ({ sca
   };
  
   return (
-    <div className="flex flex-row align-center justify-center w-min gap-2 absolute z-10 bottom-2 right-2 bg-bg-color py-2 px-3 rounded shadow">
+    <div className={`flex flex-row align-center justify-center w-min gap-2 absolute z-10 bottom-2 right-2 bg-bg-color py-2 px-3 rounded shadow ${isFullScreen ? "opacity-0 hover:opacity-100 transition duration-1000 ease-in-out " : ""} `}>
       {isFullScreen ? <MdFullscreenExit onClick={toggleFullScreen}  className="cursor-pointer hover:text-rose-500 mt-1" size={20} /> :
         <MdFullscreen onClick={toggleFullScreen} className="cursor-pointer hover:text-fuchsia-700 mt-1" size={20} />}
 
